d7be38a85643ca019bcc9135f62f45e735665bd1
Abdullah Mahrous <rbkhg3@gmail.com>
2026-06-24 18:33:06 +0300

    editing paths for vercel

--- api/index.js
+++ api/index.js
@@ -1,6 +1,6 @@
 const app = require("../src/app");
-const { connectDB } = require("./src/config/database");
-const logger = require("./src/utils/logger");
+const { connectDB } = require("../src/config/database");
+const logger = require("../src/utils/logger");
 
 const startServer = async () => {
   try {
