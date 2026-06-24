47fb3ab9ea4c7a99b0be1e7f2ee7e1970b53c128
Abdullah Mahrous <rbkhg3@gmail.com>
2026-06-24 18:17:22 +0300

    vercel deployment changes

---
+++ api/index.js
@@ -0,0 +1,16 @@
+const app = require("../src/app");
+const { connectDB } = require("./src/config/database");
+const logger = require("./src/utils/logger");
+
+const startServer = async () => {
+  try {
+    await connectDB();
+  } catch (error) {
+    logger.error("Failed to start server:", error.message);
+    throw new Error("Error while connecting DB");
+  }
+};
+
+startServer();
+
+module.exports = app;
